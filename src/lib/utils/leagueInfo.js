59c4d7914ee037fa7884ee6ee9b9d52e5a0f9cd9
GhostOfThePlains <143657099+GhostOfThePlains@users.noreply.github.com>
2023-08-30 16:18:12 -0500

    Update leagueInfo.js
    Updated the League name to be more accurate.

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
 export const leagueID = "988938042922610688"; // your league ID
-export const leagueName = "Old Phis League"; // your league name
+export const leagueName = "The Old Phis League"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
